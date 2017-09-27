77719c8c91369403fd023f5dd028bdb7ad385b31
Julio Jaramillo <julio.jaramillo@chromeriver.com>
2017-09-27 16:42:34 -0500

    First commit

---
+++ src/configureStore.js
@@ -0,0 +1,29 @@
+import { createStore, compose, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+import { routerReducer } from 'react-router-redux';
+
+const configureStore = (preloadedState) => {
+  const enhancers = [applyMiddleware(thunk)];
+
+  if (process.env.NODE_ENV === 'development') {
+    const devToolsExtension = window.devToolsExtension
+
+    if (typeof devToolsExtension === 'function') {
+      enhancers.push(devToolsExtension());
+    }
+  }
+
+  return createStore(
+    combineReducers({
+      ...reducers,
+      routing: routerReducer
+    }),
+    preloadedState,
+    compose(
+      ...enhancers
+    )
+  );
+};
+
+export default configureStore;
